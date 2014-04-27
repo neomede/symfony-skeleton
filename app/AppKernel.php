9e4d94c7de73fdc1b1bdb6de8a3a458714bfc0b1
Rubén Simón Andreo <rsimonandreo@gmail.com>
2014-04-27 16:52:52 +0200

    add FOSRestBundle and JMSSerializerBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,6 +31,9 @@ class AppKernel extends Kernel
             //Media
             new Sonata\MediaBundle\SonataMediaBundle(),
             new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
+            //FOS REST
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
